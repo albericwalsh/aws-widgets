9590c77ebbf088331ea0991c6f063b19dcbb8f72
albericwalsh <112949955+albericwalsh@users.noreply.github.com>
2026-02-13 19:24:12 +0100

    chore: stop tracking dist; artifacts are produced by CI releases

--- dist/aws-widgets.cjs.js
+++
@@ -1,1 +0,0 @@
-"use strict";Object.defineProperties(exports,{__esModule:{value:!0},[Symbol.toStringTag]:{value:"Module"}});const e=require("./license-BuCx_UK2.cjs");exports.default=e.undefined;exports.initWidgets=e.initWidgets;
