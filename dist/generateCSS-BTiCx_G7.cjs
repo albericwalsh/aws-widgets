8398b878ee66a60e011673e90c718873c4379c91
albericwalsh <112949955+albericwalsh@users.noreply.github.com>
2026-02-13 19:30:04 +0100

    chore: remove dist from tracking after tagging

--- dist/generateCSS-BTiCx_G7.cjs
+++
@@ -1,4 +0,0 @@
-"use strict";Object.defineProperty(exports,Symbol.toStringTag,{value:"Module"});function t(e){return e&&e.widgets&&e.widgets.input,`
-        #value{ font: inherit; color: inherit; }
-        #copy{ margin-left:8px; }
-    `}const n={generateCSS:t};exports.default=n;exports.generateCSS=t;
--- dist/generateCSS-hI8KEkse.cjs
+++
@@ -1,7 +0,0 @@
-"use strict";Object.defineProperty(exports,Symbol.toStringTag,{value:"Module"});function e(t){return`
-        #value{ font: inherit; color: inherit; }
-        #copy{ margin-left:8px; }
-        .favicon{ width:24px; height:24px; border-radius:50%; overflow:hidden; display:inline-flex; align-items:center; justify-content:center; margin-right:6px; background:rgba(255,255,255,0.04) }
-        .favicon img{ width:100%; height:100%; object-fit:cover; display:block }
-        .favicon.no-favicon .fallback-emoji{ font-size:16px; line-height:1 }
-    `}const i={generateCSS:e};exports.default=i;exports.generateCSS=e;
